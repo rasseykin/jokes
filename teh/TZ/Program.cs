1eea0beb9bda26d28f815a6a861c7cdef34fc680
Rasseykin <rasseykin98@mail.ru>
2019-05-14 08:43:19 +0500

    Ref

--- teh/TZ/Program.cs
+++ teh/TZ/Program.cs
@@ -24,13 +24,21 @@ namespace TZ
             char symbChar = Convert.ToChar(symbolUpper);//перевод символа в тип char
 
             char[] ar = strUpper.ToCharArray();//перевод строки(которая уже преобразована в большие буквы) в массив, где каждый символ строки - элемент массива с типом char
+            SearchSymb(ref count, ref count1, symbChar, ar);
+            Console.WriteLine("Слов, начинающихся с буквы " + symbolUpper + ": " + count);//вывод результата
+            Console.WriteLine("Всего слов в строке: " + count1);
+            Console.ReadKey();
+        }
+
+        private static void SearchSymb(ref int count, ref int count1, char symbChar, char[] ar)
+        {
             if (ar[0] == symbChar)//если первый символ соответвует введенному нами символу, то к общему счётчику прибавляется 1
             {
                 count++;
             }
             for (int i = 1; i < ar.Length; i++)//далее уже начиная со второго символа строки(т.е. 1 в программировании) идем по каждому элементу массива
             {
-                if ((ar[i] == symbChar) && ar[i-1] == ' ')//если элемент массива соответвует нашей букве, и при этом перед ним стоит пробел, значит это отдельное слово, значит к счётчику прибавляется 1
+                if ((ar[i] == symbChar) && ar[i - 1] == ' ')//если элемент массива соответвует нашей букве, и при этом перед ним стоит пробел, значит это отдельное слово, значит к счётчику прибавляется 1
                 {
                     count++;
                 }
@@ -40,9 +48,6 @@ namespace TZ
                 }
             }
             count1++;
-            Console.WriteLine("Слов, начинающихся с буквы " + symbolUpper + ": " + count);//вывод результата
-            Console.WriteLine("Всего слов в строке: " + count1);
-            Console.ReadKey();
         }
     }
 }
